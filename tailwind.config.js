6d781f74f8f07b490617e99235de9ca7d5decf2d
mohamedazizmoumni <mohamedaziz.moumni@esprit.tn>
2025-03-05 02:21:27 +0100

    Initial commit - Added project files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff6b81",
+        secondary: "#ff4757",
+        accent: "#1dd1a1",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
